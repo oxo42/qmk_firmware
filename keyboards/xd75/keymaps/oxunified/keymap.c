670bf6fa1e69895dba45cc79d3d999a767871c46
John Oxley <john.oxley@gmail.com>
2017-11-09 17:51:52 +0000

    .

--- keyboards/xd75/keymaps/oxunified/keymap.c
+++ keyboards/xd75/keymaps/oxunified/keymap.c
@@ -26,42 +26,39 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* QWERTY
+[_QW] = { /* QWERTY
  * .--------------------------------------------------------------------------------------------------------------------------------------.
  * | `      | 1      | 2      | 3      | 4      | 5      | 6      | 7      | 8      | 9      | 0      | -      | =      | BACKSP | INSERT |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
- * | TAB    | Q      | W      | E      | R      | T      | [      | #      | ]      | Y      | U      | I      | O      | P      | \      |
+ * | TAB    | Q      | W      | E      | R      | T      | Y      | U      | ]      | Y      | U      | I      | O      | P      | \      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
  * | CAP LK | A      | S      | D      | F      | G      | HOME   | DEL    | PG UP  | H      | J      | K      | L      | ;      | '      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
  * | LSHIFT | Z      | X      | C      | V      | B      | END    | UP     | PG DN  | N      | M      | ,      | .      | /      | RSHIFT |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
  * | LCTRL  | LGUI   | LALT   | FN     | BACKS  | LGUI   | LEFT   | DOWN   | RIGHT  | ENTWE  | SPACE  | FN     | RALT   | RGUI   | RCTRL  |
- * '--------------------------------------------------------------------------------------------------------------------------------------'
- */
-
- [_QW] = { /* QWERTY */
-  { KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,     KC_EQL,      KC_BSPC,  KC_BSPC },
-  { TAB_FN,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRACKET, KC_RBRACKET, KC_NUBS,  KC_INSERT },
-  { CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,     KC_NUHS,     KC_ENTER, KC_ENTER },
-  { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,     KC_RSFT,     KC_UP,    KC_DELETE },
-  { KC_LCTL, KC_NUBS, MO(_FN), KC_LGUI, KC_LALT, KC_SPC,  KC_SPC, KC_SPC,  KC_RALT, KC_RGUI, KC_HOME, KC_END,      KC_LEFT,     KC_DOWN,  KC_RIGHT },
+ * '--------------------------------------------------------------------------------------------------------------------------------------' */
+  { KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC },
+  { TAB_FN,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_NUBS, KC_INS  },
+  { CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,  KC_ENT  },
+  { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RSFT, KC_UP,   KC_DEL  },
+  { KC_LCTL, KC_NUBS, MO(_FN), KC_LGUI, KC_LALT, KC_SPC,  KC_SPC,  KC_SPC,  KC_RALT, KC_RGUI, KC_HOME, KC_END,  KC_LEFT, KC_DOWN, KC_RGHT },
  },
 
- [_FN] = { /* FUNCTION
- * .-------------------------------------------------------------------------------------------------------------------------------------.
+[_FN] = { /* FUNCTION
+ * .--------------------------------------------------------------------------------------------------------------------------------------.
  * | RESET  | F1     | F2     | F3     | F4     | F5     | F6     | F7     | F8     | F9     | F10    | F11    | F12    |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * |        | CALC   | MYCOMP | MAIL   |        |        |        | P7     | P8     | P9     | -      |        |        | SCR LK | PR SCR |
+ * |        | CALC   | MYCOMP | MAIL   |        |        |        | P7     | P8     | P9     | -      |        | CAPS   | SCR LK | PR SCR |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * |        | PLAY   | NEXT   | STOP   |        |        |        | P4     | P5     | P6     | +      |        |        |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * |        | PREV   | PLAY   | NEXT   | VOL-   | VOL+   |        | P1     | P2     | P3     | PENT   |        |        | PG UP  |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * | RESET  |        |        | FN     |        |        |        | P0     |        | P.     | PENT   |        | HOME   | PG DN  | END    |
- * '-------------------------------------------------------------------------------------------------------------------------------------' */
+ * '--------------------------------------------------------------------------------------------------------------------------------------' */
   { RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS },
-  { KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P7,   KC_P8,   KC_P9,   KC_MINS, KC_TRNS, KC_TRNS, KC_SLCK, KC_PSCR },
+  { KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P7,   KC_P8,   KC_P9,   KC_MINS, KC_TRNS, KC_CAPS, KC_SLCK, KC_PSCR },
   { KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P6,   KC_P5,   KC_P4,   KC_PLUS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS },
   { KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, KC_VOLU, KC_TRNS, KC_P3,   KC_P2,   KC_P1,   KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_TRNS },
   { KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_KP_0, KC_TRNS, KC_PDOT, KC_PENT, KC_TRNS, KC_HOME, KC_PGDN, KC_END  },
